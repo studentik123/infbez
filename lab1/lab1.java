e6aacf92489abfe2189562a891ebe739cff1345a
studentik123 <95306413+studentik123@users.noreply.github.com>
2022-03-19 15:41:46 +0300

    Update lab1.java

--- lab1/lab1.java
+++ lab1/lab1.java
@@ -73,33 +73,3 @@ public static int methodFrequencyAnalysis(String sourceText, String encryptText)
     return Math.abs(sourceMaxChar - encryptMaxChar);
 }
 }
-package com.company;
-
-class lab2{
-
-public static void main(String[] args)
-{
-    long P, G, x, a, y, b, kaa, kbb;
-    P = 27;
-    System.out.println("Значение P:" + P);
-    G = 19;
-    System.out.println("Значение G:" + G);
-    a = 7;
-    System.out.println("Приватный ключ Алисы:" + a);
-    x = DiffieHellmanpower(G, a, P);
-    b = 8;
-    System.out.println("Приватный ключ Боба:" + b);
-    y = DiffieHellmanpower(G, b, P);
-    ka = DiffieHellmanpower(y, a, P);
-    kb = DiffieHellmanpower(x, b, P);
-    System.out.println("Секретннный ключ Алисы:" + ka);
-    System.out.println("Секретный ключ Боба:" + kb);
-    }
-private static long DiffieHellmanpower(long a, long b, long p)
-{
-    if (b == 1)
-    return a;
-    else
-    return (((long)Math.pow(a, b)) % p);
-}
-}
